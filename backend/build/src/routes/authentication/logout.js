35cd1e99272dbaefff7c30b59df715c7e4f0baae
Abdul-Majeed Nurudeen <abdulmajeednurudeen48@gmail.com>
2025-08-21 15:23:36 +0000

    Remove build directory from tracking

--- backend/build/src/routes/authentication/logout.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// src/routes/authentication/logout.ts
-const express_1 = require("express");
-const index_1 = require("../../controllers/authentication/index");
-const logoutRoutes = (0, express_1.Router)();
-logoutRoutes.post('/logout', index_1.logout);
-exports.default = logoutRoutes;
--- backend/build/src/routes/authentication/register.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// src/routes/authentication/register.ts
-const express_1 = require("express");
-const index_1 = require("../../controllers/authentication/index");
-const registerRoutes = (0, express_1.Router)();
-registerRoutes.post('/register-user', ...index_1.registerUser);
-exports.default = registerRoutes;
