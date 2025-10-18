04017bce3b9a9c69ebee7f00f8436e13ce937220
Abdulmajeed Nurudeen <abdulmajeednurudeen48@gmail.com>
2025-10-18 22:50:43 +0000

    Stop tracking build directory and add to .gitignore

--- backend/build/src/controllers/authentication/logout.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const CookieManager_1 = require("../../utils/CookieManager");
-const logout = (req, res) => {
-    CookieManager_1.CookieManager.clearAllTokens(res);
-    res.status(200).json({ message: 'Logged out successfully' });
-};
-exports.default = logout;
--- backend/build/src/routes/authentication/login.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// src/routes/authentication/login.ts
-const express_1 = require("express");
-const index_1 = require("../../controllers/authentication/index");
-const loginRoutes = (0, express_1.Router)();
-loginRoutes.post('/login', index_1.login);
-exports.default = loginRoutes;
