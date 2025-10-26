78648f44e1ee0572cfd45d7396ef3d498619cf8a
Abdulmajeed Nurudeen <abdulmajeednurudeen48@gmail.com>
2025-10-26 04:59:32 +0000

    update seed file

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -1,12 +1,12 @@
 // prisma/seed.ts
-import { Role } from '../generated/prisma/client';
-import prisma from '../src/config/prismaClient';
-import * as bcrypt from 'bcrypt';
-import logger from '../src/utils/logger';
-import ENV from '../src/config/env';
+import { Role } from "../generated/prisma/client";
+import prisma from "../src/config/prismaClient";
+import * as bcrypt from "bcrypt";
+import logger from "../src/utils/logger";
+import ENV from "../src/config/env";
 
 async function main() {
-  logger.info('🌱 Starting database seeding...');
+  logger.info("🌱 Starting database seeding...");
 
   const adminEmail = ENV.ADMIN_EMAIL;
   const adminPassword = ENV.ADMIN_PASSWORD;
@@ -34,7 +34,7 @@ async function main() {
   });
 
   logger.info({
-    message: '✅ Admin user seeded successfully',
+    message: "✅ Admin user seeded successfully.",
     admin: {
       id: admin.id,
       email: admin.email,
@@ -47,7 +47,7 @@ async function main() {
 
 main()
   .catch((e) => {
-    logger.error('❌ Error seeding database:', e);
+    logger.error("❌ Error seeding database:", e);
     process.exit(1);
   })
   .finally(async () => {
